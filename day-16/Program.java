433cac80d6548255c9b8eccf4d90647c6c818222
Michiel Koning <github@michiel.ion3.nl>
2021-06-02 23:04:23 +0200

    some clean up for day 16

--- day-16/Program.java
+++ day-16/Program.java
@@ -12,37 +12,35 @@ public class Program {
   static BiPredicate<Integer, Integer> lessThan = (x, y) -> x < y;
   static BiPredicate<Integer, Integer> moreThan = (x, y) -> x > y;
   static BiPredicate<Integer, Integer> equals = (x, y) -> x == y;
+  static Map<String, Integer> targets = Map.of(
+    "children", 3, "cats", 7, "samoyeds", 2, "pomeranians", 3, "akitas", 0,
+    "vizslas", 0, "goldfish", 5, "trees", 3, "cars", 2, "perfumes", 1
+  );
+  static Map<String, BiPredicate<Integer, Integer>> part1Matchers = Map.of();
+  static Map<String, BiPredicate<Integer, Integer>> part2Matchers = Map.of(
+    "cats", moreThan, "trees", moreThan, "pomeranians", lessThan, "goldfish", lessThan
+  );
 
   public static void main(String... args) throws java.io.IOException {
     var inputLines = Files.readAllLines(Path.of("../input/day16.txt"));
     var sues = inputLines.stream()
       .map(SueParser::parse).map(Optional::orElseThrow).collect(toList());
+
     System.out.println("Part 1:");
-    var targets = Map.of(
-      "children", 3, "cats", 7, "samoyeds", 2, "pomeranians", 3, "akitas", 0,
-      "vizslas", 0, "goldfish", 5, "trees", 3, "cars", 2, "perfumes", 1
-    );
-    var part1Matchers = Map.<String, BiPredicate<Integer, Integer>>of();
-    System.out.println(
-      sues.stream()
-        .filter(s -> matches(targets, part1Matchers, s))
-        .map(s -> s.number())
-        .findFirst().orElseThrow()
-    );
+    System.out.println(findSue(sues, getTargetMatcher(targets, part1Matchers)).number());
+
     System.out.println("Part 2:");
-    var part2Matchers = Map.of("cats", moreThan, "trees", moreThan, "pomeranians", lessThan, "goldfish", lessThan);
-    System.out.println(
-      sues.stream()
-        .filter(s -> matches(targets, part2Matchers, s))
-        .map(s -> s.number())
-        .findFirst().orElseThrow()
-    );
+    System.out.println(findSue(sues, getTargetMatcher(targets, part2Matchers)).number());
   }
 
-  public static boolean matches(
-    Map<String, Integer> targets, Map<String, BiPredicate<Integer, Integer>> matchers, Sue sue
+  public static Sue findSue(List<Sue> sues, Predicate<Sue> theRealSue) {
+    return sues.stream().filter(theRealSue::test).findFirst().orElseThrow();
+  }
+
+  public static Predicate<Sue> getTargetMatcher(
+    Map<String, Integer> targets, Map<String, BiPredicate<Integer, Integer>> matchers
   ) {
-    return sue.propertyCounts().entrySet().stream()
+    return sue -> sue.propertyCounts().entrySet().stream()
       .allMatch(
         e ->  matchers.getOrDefault(e.getKey(), equals).test(e.getValue(), targets.get(e.getKey()))
       );
@@ -64,13 +62,9 @@ class SueParser {
       .map(number -> new Sue(number, properties(numberAndProperties[1])));
   }
 
-  static String[] splitOnColon(String val) {
-    return colonPattern.split(val, 2);
-  }
+  static String[] splitOnColon(String val) { return colonPattern.split(val, 2); }
 
-  static String[] words(String sentence) {
-    return wordsPattern.split(sentence);
-  }
+  static String[] words(String sentence) { return wordsPattern.split(sentence); }
 
   static Map<String, Integer> properties(String spec) {
     return Arrays.stream(propertiesPattern.split(spec))
@@ -80,10 +74,7 @@ class SueParser {
   }
 
   static Optional<Integer> tryParseInt(String val) {
-    try {
-      return of(Integer.parseInt(val));
-    } catch (NumberFormatException n) {
-      return empty();
-    }
+    try { return of(Integer.parseInt(val)); }
+    catch (NumberFormatException n) { return empty(); }
   }
 }
